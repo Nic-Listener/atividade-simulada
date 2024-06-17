4ede2859e1664852ceabd6772d4c04d56bc94bf7
Nic-Listener <nicolados10@gmail.com>
2024-06-17 08:18:10 -0300

    feat(interface) - definição da interface

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import { Introducao } from './components/Header';
+
+export default function Home() {
+  return (
+  <>
+  <Header />
+  <main>
+    <Introducao/>
+  </main>
+  </>
+  );
+}
