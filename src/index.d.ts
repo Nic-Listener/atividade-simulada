eef9c938fd5b5376f7c523c12d0d347aeed68afb
Nic-Listener <nicolados10@gmail.com>
2024-06-17 08:53:36 -0300

    feat(interface - menu) - criação do componente footer

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Esse arquivo permite importar imagens existentes no projeto react como 'modulos',
+ * o que facilita seu uso com a tag <Image> do Next.js
+ */
+
+declare module "*.jfif"
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.webp";
