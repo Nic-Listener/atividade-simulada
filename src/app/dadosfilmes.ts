eafe2bd3fdf13b5d79475e2940616858e4b74026
Nic-Listener <nicolados10@gmail.com>
2024-06-17 09:17:47 -0300

    feat: created movielist component and dadosfilmes

---
+++ src/app/dadosfilmes.ts
@@ -0,0 +1,11 @@
+import interestelar from "../../public/interestellar.png";
+
+export const filmes = 
+    {
+        titulo: "Interestelar",
+        imagemFilme: interestelar,
+        descricao: [
+        "Lara está prestes a começar uma nova fase de sua vida após a formatura. Em meio a duas viagens marcantes, ela vai reavaliar como poderão ficar suas relações familiares, a ligação com os amigos e o vínculo com seu namorado",
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos vitae qui eos itaque tenetur ducimus dolores dolorem sunt ipsa? Delectus, id illo similique temporibus quia praesentium. Quaerat, perferendis? Incidunt, necessitatibus!",
+        ],
+    },
